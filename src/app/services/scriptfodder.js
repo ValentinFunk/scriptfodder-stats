2297744d3ccd7af9b5d390c7e5f46c5ae48e85ef
William Venner <william@venner.io>
2017-10-13 11:01:04 +0100

    Fix URLs

--- src/app/services/scriptfodder.js
+++ src/app/services/scriptfodder.js
@@ -19,13 +19,13 @@ angular
 
     var initApi = function() {
       ScriptFodder.Scripts = $resource(
-        'https://gmodstore.com/api/scripts/info/:scriptId?api_key=' +
+        'https://www.gmodstore.com/api/scripts/info/:scriptId?api_key=' +
           $localStorage.apiKey,
         { scriptId: '@id' },
         {
           query: {
             method: 'GET',
-            url: 'https://gmodstore.com/api/scripts?api_key=' +
+            url: 'https://www.gmodstore.com/api/scripts?api_key=' +
               $localStorage.apiKey,
             isArray: true,
             transformResponse: appendTransform(
@@ -46,7 +46,7 @@ angular
           },
           purchases: {
             method: 'GET',
-            url: 'https://gmodstore.com/api/scripts/purchases/:scriptId?api_key=' +
+            url: 'https://www.gmodstore.com/api/scripts/purchases/:scriptId?api_key=' +
               $localStorage.apiKey,
             isArray: true,
             transformResponse: appendTransform(
